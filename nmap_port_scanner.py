291e094750f91802bb2c8b8047b4db5c0901d647
Yael Palacios <67619378+YayoPalacios@users.noreply.github.com>
2022-04-17 00:03:03 -0500

    Update nmap_port_scanner.py

--- nmap_port_scanner.py
+++ nmap_port_scanner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#Use these commands in Kali to install required software:
+#Use these commands to install required software:
 #  sudo apt install python3-pip
 #  pip install python-nmap
 
