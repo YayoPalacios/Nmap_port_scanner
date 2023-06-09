93f8388f0029263a3cc39b16cbf3d3c9a3ed020a
Yael Palacios <67619378+YayoPalacios@users.noreply.github.com>
2023-06-08 21:24:57 -0600

    Tidying up comments

--- nmap_port_scanner.py
+++ nmap_port_scanner.py
@@ -1,66 +1,38 @@
 #!/usr/bin/env python3
-#Use these commands to install required software:
-#  sudo apt install python3-pip
-#  pip install python-nmap
 
-# Import nmap so we can use it for the scan
 import nmap
-# We import the ipaddress module. We want to use the ipaddress.ip_address(address)
-# method to see if we can instantiate a valid ip address to test.
-import ipaddress
-# We need to create regular expressions to ensure that the input is correctly formatted.
-import re
+import ipaddress  #Importing for IP address validation
+import re  #Importing module for regular expressions
 
-# Regular Expression Pattern to extract the number of ports you want to scan. 
-# You have to specify <lowest_port_number>-<highest_port_number> (ex 10-100)
-port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
-# Initialising the port numbers, will be using the variables later on.
-port_min = 0
-port_max = 65535
+port_range_pattern = re.compile("([0-9]+)-([0-9]+)")  #Regular expression pattern for extracting port range
+port_min = 0  #Initializing minimum port number
+port_max = 65535  #Initializing maximum port number
 
-# This port scanner uses the Python nmap module.
-# You'll need to install the following to get it work on Linux:
-# Step 1: sudo apt install python3-pip
-# Step 2: pip install python-nmap
-
-# Ask user to input the ip address they want to scan.
 while True:
-    ip_add_entered = input("\nPlease enter the ip address that you want to scan: ")
-    # If we enter an invalid ip address the try except block will go to the except block and say you entered an invalid ip address.
+    ip_add_entered = input("\nPlease enter the IP address you want to scan: ")
     try:
-        ip_address_obj = ipaddress.ip_address(ip_add_entered)
-        # The following line will only execute if the ip is valid.
-        print("You entered a valid ip address.")
+        ip_address_obj = ipaddress.ip_address(ip_add_entered)  #Validating the entered IP address
+        print("You entered a valid IP address.")
         break
     except:
-        print("You entered an invalid ip address")
+        print("You entered an invalid IP address")
 
+#This scanner is basic so scanning all the ports is not advised.
 while True:
-    # You can scan 0-65535 ports. This scanner is basic and doesn't use multithreading so scanning all the ports is not advised.
-    print("Please enter the range of ports you want to scan in format: <int>-<int> (ex would be 60-120)")
+    print("Please enter the range of ports you want to scan in the format: <int>-<int> (e.g., 60-120)")
     port_range = input("Enter port range: ")
-    # We pass the port numbers in by removing extra spaces that people sometimes enter. So if you enter 80 - 90 instead of 80-90 the program will still work.
-    port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
+    port_range_valid = port_range_pattern.search(port_range.replace(" ", ""))  #Removing extra spaces from the input
     if port_range_valid:
-        # We're extracting the low end of the port scanner range the user want to scan.
-        port_min = int(port_range_valid.group(1))
-        # We're extracting the upper end of the port scanner range the user want to scan.
-        port_max = int(port_range_valid.group(2))
+        port_min = int(port_range_valid.group(1))  #Extracting the lower end of the port range
+        port_max = int(port_range_valid.group(2))  #Extracting the upper end of the port range
         break
 
-nm = nmap.PortScanner()
-# We're looping over all of the ports in the specified range.
+nm = nmap.PortScanner()  #Creating a new nmap PortScanner instance
+
 for port in range(port_min, port_max + 1):
     try:
-        # The result is quite interesting to look at. You may want to inspect the dictionary it returns. 
-        # It contains what was sent to the command line in addition to the port status we're after. 
-        # For in nmap for port 80 and ip 10.0.0.2 you'd run: nmap -oX - -p 89 -sV 10.0.0.2
-        result = nm.scan(ip_add_entered, str(port))
-        # Uncomment following line and look at dictionary
-        # print(result)
-        # We extract the port status from the returned object
-        port_status = (result['scan'][ip_add_entered]['tcp'][port]['state'])
+        result = nm.scan(ip_add_entered, str(port))  #Scanning the specified port on the IP address
+        port_status = result['scan'][ip_add_entered]['tcp'][port]['state']  #Extracting the port status from the result
         print(f"Port {port} is {port_status}")
     except:
-        # We cannot scan some ports and this ensures the program doesn't crash when we try to scan them.
         print(f"Cannot scan port {port}.")
